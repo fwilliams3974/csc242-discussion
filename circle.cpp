efaebd887a91b5becf1d171a61a64892272f2a33
Frederick Williams <f.williams3974@student.nu.edu>
2021-04-14 19:54:56 -0500

    Source code added

---
+++ circle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+//This program prompts user to enter circle raduis
+//Then the circle area is displayed
+
+int main()
+{
+    setiosflags(ios_base::fixed);
+	const double PI = 3.142;
+	double radius;
+	cout << "Enter radius" << endl;
+	cin >> radius;
+	//Add code to read user input and store in variable radius
+	cout << "The area of a circle with radius " << radius << " is " <<endl;
+	//Calculate the area of the circle
+	double area ;
+	area = PI * pow(radius, 2);
+	cout << setprecision(3) <<area;
+	return 0;
+}
